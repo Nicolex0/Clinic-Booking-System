b5926f1f62c3b32933ed89f2830448e4d65b3c1f
Nicole Apono <napononicole@gmail.com>
2025-05-14 11:27:21 +0300

    creating a table for different specializations

--- clinic-booking.sql
+++ clinic-booking.sql
@@ -3,3 +3,9 @@ CREATE DATABASE clinic_bookings;
 
 -- Use the database
 USE clinic_bookings;
+
+-- Table for specialization e.g dermatology, cardiology
+CREATE TABLE specializations (
+    specialization_id INT AUTO_INCREMENT PRIMARY KEY,         -- Unique ID for each specialization
+    name VARCHAR(100) NOT NULL UNIQUE                         -- Name of the specialization (must be unique)
+);
