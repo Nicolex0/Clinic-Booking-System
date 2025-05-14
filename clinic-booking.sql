da9d4bb1259e10a6c35c2010f3fd59dcb8cca6f7
Nicole Apono <napononicole@gmail.com>
2025-05-14 11:32:29 +0300

    Creating a table for doctors' information

--- clinic-booking.sql
+++ clinic-booking.sql
@@ -6,6 +6,16 @@ USE clinic_bookings;
 
 -- Table for specialization e.g dermatology, cardiology
 CREATE TABLE specializations (
-    specialization_id INT AUTO_INCREMENT PRIMARY KEY,         -- Unique ID for each specialization
-    name VARCHAR(100) NOT NULL UNIQUE                         -- Name of the specialization (must be unique)
+    specialization_id INT AUTO_INCREMENT PRIMARY KEY,      -- Unique ID for each specialization
+    name VARCHAR(100) NOT NULL UNIQUE           
+);
+
+-- Table for doctors
+CREATE TABLE doctors (
+    doctor_id INT AUTO_INCREMENT PRIMARY KEY,      -- Unique ID for each doctor
+    full_name VARCHAR(100) NOT NULL,                  
+    email VARCHAR(100) UNIQUE NOT NULL,            -- Email must be unique and required
+    phone VARCHAR(15),         -- Optional phone number
+    specialization_id INT,     -- Foreign key to specializations
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP        -- When the doctor was added
 );
